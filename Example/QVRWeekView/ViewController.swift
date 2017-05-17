0d413d2d88b7eebafdd950960f39dc5b13908bf4
Reinert <reilemx@gmail.com>
2017-05-17 17:26:39 +0200

    Updated podspecs, podfiles and build setting to ios 9.0

--- Example/QVRWeekView/ViewController.swift
+++ Example/QVRWeekView/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import QVRWeekView
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let cV = CalendarView(frame: self.view.frame)
+        self.view.addSubview(cV)
     }
 
     override func didReceiveMemoryWarning() {
--- QVRWeekView.podspec
+++ QVRWeekView.podspec
@@ -26,7 +26,7 @@ s.author           = { 'Reinert' => 'reilemx@gmail.com' }
 s.source           = { :git => 'https://github.com/Quivr/iOS-Week-View.git', :tag => s.version.to_s }
 # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
-s.ios.deployment_target = '8.0'
+s.ios.deployment_target = '9.0'
 
 s.source_files = 'QVRWeekView/Classes/**/*'
 
