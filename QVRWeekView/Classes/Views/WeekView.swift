184d33fa7df6681217813b816918f99064ac76a1
Reinert <reilemx@gmail.com>
2017-08-01 00:36:29 +0200

    Added public events property to weekview
    Pod version 0.5.3

--- QVRWeekView/Classes/Views/WeekView.swift
+++ QVRWeekView/Classes/Views/WeekView.swift
@@ -30,10 +30,28 @@ open class WeekView: UIView {
     @IBOutlet var topLeftBufferWidthConstraint: NSLayoutConstraint!
     @IBOutlet var topLeftBufferHeightConstraint: NSLayoutConstraint!
 
-    // MARK: - VARIABLES -
+    // MARK: - PUBLIC PROPERTIES -
 
     // WeekView Delegate
     public weak var delegate: WeekViewDelegate?
+    // Computed events property
+    public var events: [EventData] {
+        get {
+            var events: [EventData] = []
+            for (_, array) in self.dayScrollView.allEventsData {
+                for (_, data) in array {
+                    events.append(data)
+                }
+            }
+            return events
+        }
+        set(events) {
+            self.dayScrollView.loadAndProcessEvents(events)
+        }
+    }
+
+    // MARK: - PRIVATE VARIABLES -
+
     // The actual view being displayed, all other views are subview of this mainview
     private(set) var mainView: UIView!
     // Array of all daylabels
