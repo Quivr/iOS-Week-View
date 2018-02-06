0e586f60114c73c8832b6c4d2dd355d3f82bc6cb
Viktor Lesyk <vee.lesyk@gmail.com>
2018-02-06 13:31:29 +0100

    Create: new project which can build as a framework

---
+++ QVRWeekView/QVRWeekView/QVRWeekView.h
@@ -0,0 +1,19 @@
+//
+//  QVRWeekView.h
+//  QVRWeekView
+//
+//  Created by Viktor Lesyk on 2/6/18.
+//  Copyright © 2018 Viktor Lesyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QVRWeekView.
+FOUNDATION_EXPORT double QVRWeekViewVersionNumber;
+
+//! Project version string for QVRWeekView.
+FOUNDATION_EXPORT const unsigned char QVRWeekViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QVRWeekView/PublicHeader.h>
+
+
