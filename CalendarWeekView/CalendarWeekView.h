2ee2e822cb0cc4e91220fdf597e919001da57cb0
Viktor Lesyk <vee.lesyk@gmail.com>
2018-02-07 10:25:34 +0100

    Fix

---
+++ CalendarWeekView/CalendarWeekView.h
@@ -0,0 +1,19 @@
+//
+//  CalendarWeekView.h
+//  CalendarWeekView
+//
+//  Created by Viktor Lesyk on 2/7/18.
+//  Copyright © 2018 Viktor Lesyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CalendarWeekView.
+FOUNDATION_EXPORT double CalendarWeekViewVersionNumber;
+
+//! Project version string for CalendarWeekView.
+FOUNDATION_EXPORT const unsigned char CalendarWeekViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalendarWeekView/PublicHeader.h>
+
+
