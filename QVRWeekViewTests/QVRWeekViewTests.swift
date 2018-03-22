50d440f8da6b8327443ae925eac876d49bc959c2
Reinert <reilemx@gmail.com>
2018-03-22 16:20:42 +0100

    Style fixes, deployment to 9.0

--- QVRWeekViewTests/QVRWeekViewTests.swift
+++ QVRWeekViewTests/QVRWeekViewTests.swift
@@ -1,13 +1,13 @@
-import XCTest
 @testable import QVRWeekView
+import XCTest
 
 class QVRWeekViewTests: XCTestCase {
-    
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
