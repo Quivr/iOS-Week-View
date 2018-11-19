0c548c85e52b6c4b3085de60734669e043a94a33
Reinert <reilemx@gmail.com>
2018-11-19 21:51:37 +0100

    Fix all warning and swiftlint issues

--- QVRWeekView/Classes/Common/Util.swift
+++ QVRWeekView/Classes/Common/Util.swift
@@ -141,7 +141,7 @@ extension TextVariables {
 // Helper function inserted by Swift 4.2 migrator.
 fileprivate func convertToOptionalNSAttributedStringKeyDictionary(_ input: [String: Any]?) -> [NSAttributedString.Key: Any]? {
 	guard let input = input else { return nil }
-	return Dictionary(uniqueKeysWithValues: input.map { key, value in (NSAttributedString.Key(rawValue: key), value)})
+	return Dictionary(uniqueKeysWithValues: input.map { key, value in (NSAttributedString.Key(rawValue: key), value) })
 }
 
 // Helper function inserted by Swift 4.2 migrator.
